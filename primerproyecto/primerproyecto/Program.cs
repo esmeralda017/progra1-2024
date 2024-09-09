eb33cf6b1bdd8d76d7e78ed7c7f5a670d93430de
Olimpia Rivas <usss017124@ugb.edu.sv>
2024-09-09 17:07:46 -0600

    Suma de dos numeros pero con el tipo sbyte

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
+
 namespace miPrimerProyecto
 {
     internal class Program
@@ -11,12 +13,18 @@ namespace miPrimerProyecto
         {
             Console.Write("Num1: ");
             int num1 = int.Parse(Console.ReadLine());
+            sbyte num1 = sbyte.Parse(Console.ReadLine());
 
             Console.Write("Num 2: ");
             int num2 = int.Parse(Console.ReadLine());
+            Console.Write("Num2: ");
+            sbyte num2 = sbyte.Parse(Console.ReadLine());
 
             int respuesta = num1 + num2;
             Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+            int suma = num1 + num2;
+
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, suma);
 
             //Pausa.
             Console.ReadLine();
