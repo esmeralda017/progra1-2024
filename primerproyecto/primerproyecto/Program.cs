5ada369c8a605d7b7e7ae3d221710ae8e27ef619
Olimpia Rivas <usss017124@ugb.edu.sv>
2024-09-09 17:10:56 -0600

    tipos de datos de C#

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace miPrimerProyecto
 {
     internal class Program
@@ -12,20 +11,23 @@ namespace miPrimerProyecto
         static void Main(string[] args)
         {
             Console.Write("Num1: ");
-            int num1 = int.Parse(Console.ReadLine());
             sbyte num1 = sbyte.Parse(Console.ReadLine());
+            Boolean comprobar = true;
+            Console.WriteLine("El valor es: {0}", comprobar);
 
-            Console.Write("Num 2: ");
-            int num2 = int.Parse(Console.ReadLine());
             Console.Write("Num2: ");
             sbyte num2 = sbyte.Parse(Console.ReadLine());
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
 
-            int respuesta = num1 + num2;
-            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
             int suma = num1 + num2;
 
             Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, suma);
+            Console.WriteLine("Hola {0} bienvenido", nombre);
 
+            DateTime fechaActual = new DateTime();
+            fechaActual = DateTime.Now;
+            Console.WriteLine("La fecha actual es: {0}", fechaActual);
             //Pausa.
             Console.ReadLine();
         }
