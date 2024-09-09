e1968fdd3a067fb1445bd8e9f707b7a3cc3c24e1
Olimpia Rivas <usss017124@ugb.edu.sv>
2024-09-09 17:45:48 -0600

    Uso de funciones con parametros

--- primerproyecto/primerproyecto/Program.cs
+++ primerproyecto/primerproyecto/Program.cs
@@ -10,31 +10,27 @@ namespace miPrimerProyecto
     {
         static void Main(string[] args)
         {
-            //determinar si cada numero de la serie es primo.
-
-            int[] serie = new int[] { 5, 9, 4, 6, 3, 2, 7, 11, 15, 17, 21 };
-            foreach (int num in serie)
-            {
-                int i = 1,
-                    n = 0;
-                while (i <= num && n <= 2)
-                {
-                    if (num % i == 0)
-                    {
-                        n++;//n=n+1
-                    }
-                    i++; //i=i+1
-                }
-                Console.WriteLine("EL numero {0} {1}", num, n <= 2 ? "es primo" : "NO es primo");
-            }
             //Uso de funciones...
             Console.WriteLine("La suma de 10+5={0}", suma());
+
+            Console.Write("Num 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Num 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma de {0}+{1}={2}", num1, num2, suma(num1, num2));
 
             Console.ReadLine();
         }
         static int suma()
         {
             int respuesta = 10 + 5;
+            return respuesta;
+        }
+        static int suma(int a, int b)
+        {
+            int respuesta = a + b;
             return respuesta;
         }
     }
