99f50dc6aa3b1e97b0a6148022b66681e808fd49
Olimpia Rivas <usss017124@ugb.edu.sv>
2024-09-27 17:28:07 -0600

    creacion de la rama del bloque2  y el proyecto de academica

---
+++ WorldSecurity/WorldSecurity/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorldSecurity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { }
+            private readonly string _connectionString;
+
+        public Form1(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                var command = new SqlCommand("SELECT Password, Salt FROM Users WHERE Username = @Username", connection);
+                command.Parameters.AddWithValue("@Username", username);
+
+                var reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    var storedPassword = reader["Password"].ToString();
+                    var salt = reader["Salt"].ToString();
+
+                    var hashedPassword = HashPassword(password, salt);
+
+                    if (hashedPassword == storedPassword)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private string HashPassword(string password, string salt)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var passwordBytes = Encoding.UTF8.GetBytes(password + salt);
+                var hashedBytes = sha256.ComputeHash(passwordBytes);
+
+                return BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
+  
---
+++ WorldSecurity/WorldSecurity/users.sql
@@ -0,0 +1,8 @@
+﻿ -- Create the Users table
+ CREATE TABLE users
+(
+    [Id] INT IDENTITY(1,1) PRIMARY KEY,
+    [Username] NVARCHAR(50) NOT NULL,
+    [Password] NVARCHAR(100) NOT NULL,
+    [Salt] NVARCHAR(50) NOT NULL
+) ;
